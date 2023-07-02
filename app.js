0d3f2936290e96877b8b78301dd071346e6b49eb
Alejandra Alaniz <dominguezale377@gmail.com>
2023-07-02 00:59:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// Importamos el router de libros:
+const librosRouter = require('./routes/libros');
+
+// Importamos el Middleware Error Handler:
+const errorHandler = require('./middleware/errorHandlers');
+
+app.use('/libros',librosRouter);
+app.use(errorHandler);
+app.listen(3000, ()=>{
+    console.log('Servidor iniciado en el puerto 3000');
+});
---
+++ data.js
@@ -0,0 +1,6 @@
+const libros = [
+    { id: '1', titulo: 'Bodas de odio', autor: 'Florencia Bonelli' },
+    { id: '2', titulo: 'El secreto', autor: 'Rhonda Byrne' },
+    { id: '3', titulo: 'Emociones nutritivas', autor: 'Bernardo Stamateas' }
+    ];
+    module.exports = libros;
